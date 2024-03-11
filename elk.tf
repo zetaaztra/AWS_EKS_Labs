80a8c7dd54b6eb48ec468dff9f6c376c01c3d0cc
RekhuGopal <vrchinnarathod@gmail.com>
2024-03-11 20:08:52 +0530

    Updated

--- elk.tf
+++ elk.tf
@@ -4,43 +4,11 @@ resource "kubernetes_namespace" "elk" {
   }
 }
 
-resource "null_resource" "install_kubectl" {
-  triggers = {
-    always_run = timestamp()
-  }
 
-  provisioner "local-exec" {
-    command     = "sudo apt-get update && sudo apt-get install -y kubectl"
-    working_dir = path.module
-  }
+resource "kubectl_manifest" "apply_all_elk" {
+  depends_on = [ kubernetes_namespace.elk ]
+
+  for_each = fileset("${path.module}/modules/elk", "*.yaml")
+
+  yaml_body = file("${path.module}/modules/elk/${each.value}")
 }
-
-resource "null_resource" "apply_null_resource" {
- depends_on = [
-    kubernetes_namespace.elk,
-    null_resource.install_kubectl
-  ]
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command     = "kubectl apply -f ./modules/elk"
-    working_dir = path.module
-  }
-}
-
-
-resource "null_resource" "get_services" {
-  depends_on = [null_resource.apply_null_resource]
-
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl get services -o wide  > services_output.txt"
-    working_dir = path.module
-  }
-}
-

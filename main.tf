20eda2f9ad8f0f3a7e5d8c7414b5b7209e3753f4
Zeta Aztra <mathew60666@gmail.com>
2024-08-31 17:30:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 ###### root/main.tf
-/*
 module "eks" {
   source                  = "./modules/eks"
   aws_public_subnet       = module.vpc.aws_public_subnet
@@ -12,8 +11,8 @@ module "eks" {
   scaling_desired_size    = 1
   scaling_max_size        = 1
   scaling_min_size        = 1
-  instance_types          = ["t3.large"]
-  key_pair                = "TestKeyPair"
+  instance_types          = ["t2.medium"]
+  key_pair                = "Zeta-Ubuntu-Singapore"
 }
 
 module "vpc" {
@@ -28,4 +27,3 @@ module "vpc" {
   rt_route_cidr_block     = "0.0.0.0/0"
 
 }
-*/

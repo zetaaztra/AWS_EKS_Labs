0cdb65136d2ce5207482f67191a913624b869d86
Zeta Aztra <mathew60666@gmail.com>
2024-09-05 18:12:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ module "eks" {
   scaling_desired_size    = 1
   scaling_max_size        = 1
   scaling_min_size        = 1
-  instance_types          = ["t3.large"]
-  key_pair                = "TestKeyPair"
+  instance_types          = ["t2.micro"]
+  key_pair                = "Zeta-Ubuntu-Singapore"
 }
 
 module "vpc" {
